4da5f03bbb6bbe8eb9fe98f74387d400d6104bff
Chirayu Desai <chirayu@calyxinstitute.org>
2023-03-20 16:37:30 +0000

    Revert "config: Build NavigationBarMode2ButtonOverlay"
    This reverts commit a461d3228dfe9bc8790c7ea859024fcef8c0b876.
    
    Reason for revert: It's finally broken!
    
    Issue: calyxos#1548
    Change-Id: Id9c79102d66feb7beddb9b3914387a1678bf0d83

--- config/common_mobile.mk
+++ config/common_mobile.mk
@@ -1,10 +1,6 @@
 # Inherit common mobile PixelBuilds stuff
 $(call inherit-product, vendor/pb/config/common.mk)
 
-# Customizations
-PRODUCT_PACKAGES += \
-    NavigationBarMode2ButtonOverlay
-
 # Media
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     media.recorder.show_manufacturer_and_model=true
